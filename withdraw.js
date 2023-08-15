072d3b13c4ee792120ea057c5891fcb70c93b6f8
Sabbir Ahmmed <engineersabbir.dev@gmail.com>
2023-08-15 15:26:53 +0600

    withdraw.js file updated

--- withdraw.js
+++ withdraw.js
@@ -1,15 +1,32 @@
 document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawInputElement = document.getElementById('withdraw-input');
-    const withdrawInputBalance = parseFloat(withdrawInputElement.value);
+    const withdrawInputAmountString = withdrawInputElement.value;
+    const withdrawInputAmount = parseFloat(withdrawInputAmountString);
 
-    const withdrawBalanceElement = document.getElementById('withdraw-total')
-    const withdrawBalance = parseFloat(withdrawBalanceElement.innerText)
+    const withdrawTotalElement = document.getElementById('withdraw-total')
+    const withdrawTotalString = withdrawTotalElement.innerText;
+    const withdrawTotal = parseFloat(withdrawTotalString)
 
-    withdrawBalanceElement.innerText = withdrawBalance + withdrawInputBalance;
+    withdrawInputElement.value = '';
+
+    if (isNaN(withdrawInputAmount)) {
+        alert('Please Provide a valid number.')
+        return;
+    }
 
     const balanceTotalElement = document.getElementById('balance-total');
-    const balanceTotal = parseFloat(balanceTotalElement.innerText) - withdrawInputBalance;
+    const balanceTotal = parseFloat(balanceTotalElement.innerText) - withdrawInputAmount;
+
+    if (withdrawTotal > balanceTotal) {
+        alert('Bank e eto taka nai vai');
+        return;
+    }
+    if ((balanceTotal - withdrawTotal) < 500) {
+        alert('you must stored 500 taka in your account')
+        return;
+    }
+    withdrawTotalElement.innerText = withdrawTotal + withdrawInputAmount;
+
 
     balanceTotalElement.innerText = balanceTotal;
-    withdrawInputElement.value = '';
 })
